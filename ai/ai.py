c3fa30c193e4750685ef95913a149e6ce2c196c6
Воронцов Илья Викторович <Nebmes@mail.ru>
2022-12-14 21:32:25 +0300

    add homework

---
+++ ai/ai.py
@@ -0,0 +1,27 @@
+import random
+
+from model.model import Board
+
+class Ai:
+    def __init__(self):
+        pass
+
+    def getComputerMove(self, board, computerTile):
+        possibleMoves = board.getValidMoves(computerTile)
+
+        random.shuffle(possibleMoves)
+
+        for x, y in possibleMoves:
+            if board.isOnCorner(x, y):
+                return [x, y]
+
+
+        bestScore = -1
+        for x, y in possibleMoves:
+            dupeBoard = Board(board=board.getBoardCopy())
+            dupeBoard.makeMove(computerTile, x, y)
+            score = dupeBoard.getScoreOfBoard()[computerTile]
+            if score > bestScore:
+                bestMove = [x, y]
+                bestScore = score
+        return bestMove
---
+++ main.py
@@ -0,0 +1,9 @@
+# Задание:
+# Необходимо реализовать свой проект на каком-то другом языке программирования
+# Игра Реверси
+
+from controller.controller import Controller
+
+if __name__ == '__main__':
+    controller = Controller()
+    controller.run()
